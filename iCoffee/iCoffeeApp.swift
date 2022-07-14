addaa1642e57bde8ddfd88ee5dfacda7a8e46ccd
Ochanyoro <wadaoo0223@icloud.com>
2022-07-14 10:30:38 +0900

    Initial Commit

---
+++ iCoffee/iCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoffeeApp.swift
+//  iCoffee
+//
+//  Created by 大和田一裕 on 2022/07/14.
+//
+
+import SwiftUI
+
+@main
+struct iCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
