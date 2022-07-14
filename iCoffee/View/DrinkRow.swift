4dfd6883c8690592c07b6fb2f2829429aed1beec
Ochanyoro <wadaoo0223@icloud.com>
2022-07-15 00:50:55 +0900

    [create] DrinkRow(View) and DrinkItem(SubViews)

---
+++ iCoffee/View/DrinkRow.swift
@@ -0,0 +1,38 @@
+//
+//  DrinkRow.swift
+//  iCoffee
+//
+//  Created by 大和田一裕 on 2022/07/15.
+//
+
+import SwiftUI
+
+struct DrinkRow: View {
+    
+    var categoryName: String
+    var drinks: [Drink]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            Text(self.categoryName)
+                .font(.title)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(self.drinks) { drink in
+                        DrinkItem(drink: drink)
+                            .frame(width: 300)
+                            .padding(.trailing, 30)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DrinkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        DrinkRow(categoryName: "HOT DRINKS", drinks: drinkData)
+    }
+}
